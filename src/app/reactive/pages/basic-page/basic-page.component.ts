ee7f233a60693375b00a9bafd3aa3ac931edf2a3
ivanros <ivanros@protonmail.com>
2023-09-02 17:44:16 +0200

    video233

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class BasicPageComponent {
 
+  //formulario con fromGroup
+  // public myForm:FormGroup = new FormGroup(
+  //   {
+  //     name:new FormControl('',[],[]),
+  //     price:new FormControl(0,[],[]),
+  //     inStorage:new FormControl(0,[],[]),
+  //   }
+  // );
+  
+  //Formulario con FormBuilder
+  constructor(private fb:FormBuilder){}
+
+  public myForm:FormGroup = this.fb.group({
+    name:['',[],[]],
+    price:[0,[],[]],
+    inStorage:[0,[],[]],
+
+  });
+
+  onSave():void{
+    console.log(this.myForm.value);
+    
+  }
 }
