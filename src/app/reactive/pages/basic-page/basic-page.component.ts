5e5a0b013ac76b54cef1067695afa652117bcd8a
ivanros <ivanros@protonmail.com>
2023-09-04 10:04:09 +0200

    video235

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+
+const rtx5090 = {
+  name:'RTX 5090',
+  price:2500,
+  inStorage:6
+}
+export class BasicPageComponent implements OnInit {
 
   //formulario con fromGroup
   // public myForm:FormGroup = new FormGroup(
@@ -19,16 +25,23 @@ export class BasicPageComponent {
   
   //Formulario con FormBuilder
   constructor(private fb:FormBuilder){}
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090);
+  }
 
   public myForm:FormGroup = this.fb.group({
-    name:['',[],[]],
-    price:[0,[],[]],
-    inStorage:[0,[],[]],
+    name:['',[Validators.required],[Validators.minLength(3)]],
+    price:[0,[Validators.required],[Validators.min(0)]],
+    inStorage:[0,[Validators.required],[Validators.min(0)]],
 
   });
 
   onSave():void{
+
+    if(this.myForm.invalid) return;
     console.log(this.myForm.value);
-    
+
+    this.myForm.reset( {price:0,inStorage:0})
+     
   }
 }
