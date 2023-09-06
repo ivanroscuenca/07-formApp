444b5c8a0a92e3c802d56d81bfe8a6b33345d129
ivanroscuenca <ivanros@protonmail.com>
2023-09-06 21:30:10 +0200

    video253

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,40 +1,51 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ValidatorsService } from 'src/app/shared/service/validators.service';
-import { EmailValidatorService } from 'src/app/shared/validators/email-validator.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+// import * as customValidators from '../../../shared/validators/validators';
+
+import { ValidatorsService } from '../../../shared/service/validators.service';
+import { EmailValidator } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            this.validatorsService.firstNameAndLastnamePattern
+          ),
+        ],
+      ],
+      // email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ new EmailValidator() ]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorsService.emailPattern),
+        ],
+        [this.emailValidator],
+      ],
+      username: [
+        '',
+        [Validators.required, this.validatorsService.cantBeStrider],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {}
+  );
+
   constructor(
     private fb: FormBuilder,
     private validatorsService: ValidatorsService,
-    private emailValidatorService: EmailValidatorService
+    private emailValidator: EmailValidator
   ) {}
 
-  public myForm: FormGroup = this.fb.group({
-    name: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern(this.validatorsService.firstNameAndLastnamePattern),
-      ],
-    ],
-    email: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern(this.validatorsService.emailPattern),
-      ],
-      [this.emailValidatorService],
-    ],
-    username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    password2: ['', [Validators.required]],
-  });
-
-  isValidField(field: string): any {
+  isValidField(field: string) {
     return this.validatorsService.isValidField(this.myForm, field);
   }
 
