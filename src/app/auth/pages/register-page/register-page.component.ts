847fe7ac4476f18afa03b909aec6428a56c2d18f
ivanroscuenca <ivanros@protonmail.com>
2023-09-06 20:39:14 +0200

    video252ValidatorAsync

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ValidatorsService } from 'src/app/shared/service/validators.service';
-// import * as customValidators from 'src/app/shared/validators/validators';
+import { EmailValidatorService } from 'src/app/shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -9,7 +9,8 @@ import { ValidatorsService } from 'src/app/shared/service/validators.service';
 export class RegisterPageComponent {
   constructor(
     private fb: FormBuilder,
-    private validatorsService: ValidatorsService
+    private validatorsService: ValidatorsService,
+    private emailValidatorService: EmailValidatorService
   ) {}
 
   public myForm: FormGroup = this.fb.group({
@@ -26,6 +27,7 @@ export class RegisterPageComponent {
         Validators.required,
         Validators.pattern(this.validatorsService.emailPattern),
       ],
+      [this.emailValidatorService],
     ],
     username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
